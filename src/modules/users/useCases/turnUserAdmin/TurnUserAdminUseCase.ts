b57361554057f268a93c19a9fa2a2501dfe7d00b
Vinicius <vinicius_prsilva@hotmail.com>
2022-12-31 13:42:10 -0300

    chore: Swagger

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -12,7 +12,7 @@ class TurnUserAdminUseCase {
     const user = this.usersRepository.findById(user_id);
 
     if (!user) {
-      throw new Error("This user is not exists");
+      throw new Error("User not found");
     }
 
     this.usersRepository.turnAdmin(user);
